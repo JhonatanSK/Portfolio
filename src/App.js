be6a4c7cb0203144dc9692508636f370a88966b0
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-06-24 00:58:45 -0300

    feature: developing the project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Banner from './components/Banner/Banner';
 import InfoUser from './components/InfoUser/InfoUser';
+import Projects from './components/Projects/Projects';
+import Footer from './components/Footer/Footer';
+import Project from './components/Project/Project';
+
 import './app.css';
 
 function App() {
@@ -8,6 +12,11 @@ function App() {
     <div>
       <Banner />
       <InfoUser />
+      <Projects />
+
+      {/* Colocar o component Project dentro do Projects */}
+      <Project />
+      <Footer />
     </div>
   );
 }
