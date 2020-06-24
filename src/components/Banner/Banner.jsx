59a29ca52fff6e7ddad9f5e406db110d37cecd42
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-06-23 23:22:26 -0300

    feature: developing the banner

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Banner.css';
+
+function Banner() {
+  return (
+    <div className="banner">
+        <div className="banner__word">
+            <p>FullStack Developer</p>
+        </div>
+    </div>
+  );
+}
+
+export default Banner;
