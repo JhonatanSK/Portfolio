9bcf5dafd62ded1e9a39194aea6d2d19230c17e7
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-06-24 00:57:39 -0300

    feature: developing the area info-user

--- src/components/InfoUser/InfoUser.jsx
+++ src/components/InfoUser/InfoUser.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './InfoUser.css';
+import JhonPng from '../../assets/jhon.png';
 
 function InfoUser() {
     return (
-        <div className="info-user__title">
-            <p className="info-user__title">Who Am I?</p>
+        <div className="info-user">
+            <div className="info-user__title">
+                <p className="info-user__title">Who Am I?</p>
+            </div>
+            <div className="info-user__photo">
+                <img src={JhonPng} alt="Jhon" width="100%"/>
+            </div>
+            <div className="info-user__description">
+                <p>A FullStack developer ..... a big text here</p>
+            </div>
         </div>
-
     );
   }
 
