889b71c74957d1bd0c582b04c6bf525378cff8c8
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-06-23 23:23:17 -0300

    init: developing the app portfolio

---
+++ src/components/InfoUser/InfoUser.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './InfoUser.css';
+
+function InfoUser() {
+    return (
+        <div className="info-user__title">
+            <p className="info-user__title">Who Am I?</p>
+        </div>
+
+    );
+  }
+
+export default InfoUser
