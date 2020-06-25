92c4b3963bb95bd99c0a34bc99aef4961b3d9b9a
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-06-24 21:37:47 -0300

    feature: developing the Experience component

---
+++ src/components/Experiences/Experiences.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Experiences.css';
+
+function Experiences() {
+    return (
+        <div className="experiences">
+            <div className="experiences__title">
+                <p>Experiences</p>
+            </div>
+            <div className="experiences__name">
+                <p>ETL Developer - ITAÚ UNIBANCO - Estágio (07/2019 - 01/2020)</p>
+            </div>
+            <div className="experiences__description">
+                <p>Developer of ETL tools for automating financial activities, working in all stages of software development (planning, mapping business rules, development, documentation, user manual, deployment, post-deployment)</p>
+                <p>Main Tools: Alteryx, Excel, Hadoop, Agile Methodologies</p>
+            </div>
+        </div>
+    );
+}
+
+export default Experiences;
