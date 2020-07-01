f55a21f25419851ff1ea805f56027d6f6e53a8f3
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-07-01 00:18:29 -0300

    feature: including new components in application

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React from 'react';
+import Navbar from './components/Navbar/Navbar';
 import Banner from './components/Banner/Banner';
 import InfoUser from './components/InfoUser/InfoUser';
+import Services from './components/Services/Services';
 import Projects from './components/Projects/Projects';
 import Footer from './components/Footer/Footer';
 import Project from './components/Project/Project';
+import Experiences from './components/Experiences/Experiences';
+import Formation from './components/Formation/Formation';
+import Courses from './components/Courses/Courses';
+import Contact from './components/Contact/Contact';
 
 import './app.css';
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Banner />
       <InfoUser />
+      <Services />
       <Projects />
-
-      {/* Colocar o component Project dentro do Projects */}
       <Project />
+      <Experiences />
+      <Formation />
+      <Courses />
+      <Contact />
       <Footer />
     </div>
   );
