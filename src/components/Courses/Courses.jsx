b4b974d4483f6bbce0ca0199b067bc05b688c12f
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-07-01 00:19:30 -0300

    feature: developing the component Courses for mobile phone

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -3,7 +3,7 @@ import './Courses.css';
 
 function Courses() {
     return (
-        <div className="coursesAndCertifications">
+        <div className="coursesAndCertifications" id="courses">
             <div className="course__title">
                 <p>Courses and Certifications</p>
             </div>
@@ -13,18 +13,46 @@ function Courses() {
                         <p>AceleraDev React Online - BootCamp - 80h </p>
                     </div>
                     <div className="course__description">
-                        <p>An intensive bootcamp with focus on ReactJS tecnology .....</p>
+                        <p>An intensive bootcamp focused on front-end development using tools such as ReactJS, React Rooks, Redux, TDD for Front-End development, BEM CSS, Atomic Design, CI/CD and deployment.</p>
                     </div>
                 </div>
                 <div className="course">
                     <div className="course__name">
-                        <p>Academia MasterTech Estação Hack - BootCamp - 120h </p>
+                        <p>AceleraDev Java Online - BootCamp - 80h </p>
+                    </div>
+                    <div className="course__description">
+                        <p>An intensive bootcamp focused on development applications using Java language.</p>
+                    </div>
+                </div>
+                <div className="course">
+                    <div className="course__name">
+                        <p>Academia MasterTech - BootCamp - 120h </p>
                     </div>
                     <div className="course__description">
                         <p>
-                            introduction to HTML programming,
+                            Introduction to HTML programming,
                             CSS, how to build a responsive website, frontend frameworks, versioning tools (git and github) and
-agile methodologies for the development of the final project.</p>
+                            agile methodologies for the development of the final project.</p>
+                    </div>
+                </div>
+                <div className="course">
+                    <div className="course__name">
+                        <p>Development Mobile Apps Android - MadCode - 32h </p>
+                    </div>
+                    <div className="course__description">
+                        <p>Introduction to development mobile using Kotlin Language and the main components for the development of Android screens and graphics components.</p>
+                    </div>
+                </div>
+                <div className="awards">
+                    <p className="awards__title">Awards</p>
+                    <div className="course">
+                        <div className="course__name">
+                            <p>Hackathon MasterTech + 99jobs - (12/2018) </p>
+                        </div>
+                        <div className="course__description">
+                            <p>1º Place in Hackathon MasterTech + 99jobs </p>
+                            <p>Prize: R$:1,000.00 to spend in MasterTech courses </p>
+                        </div>
                     </div>
                 </div>
             </div>
