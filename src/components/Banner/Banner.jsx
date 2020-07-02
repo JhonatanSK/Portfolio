5c2cca6d1574118651373b0366b9cf8370bcff43
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-07-01 21:56:44 -0300

    change: making a responsive banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Banner.css';
 
-function Banner() {
+function Banner() {   
   return (
     <div className="banner">
         <div className="banner__word">
-            <p>FullStack Developer</p>
+            <p className="animation-1">Hi, I´m <span>Jhonatan</span></p>
+            {/* <p className="animation-2">I´m a <span>Developer</span></p>
+            <p className="animation-3">Come to meet my work</p> */}
         </div>
     </div>
   );
