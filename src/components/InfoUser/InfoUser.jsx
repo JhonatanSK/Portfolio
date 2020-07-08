eccdbcab765cab713f0aa2929188bf6eba9e4776
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2020-07-07 22:10:43 -0300

    feature: developing the first part of Who Am I for mobile

--- src/components/InfoUser/InfoUser.jsx
+++ src/components/InfoUser/InfoUser.jsx
@@ -4,15 +4,18 @@ import JhonPng from '../../assets/jhon.png';
 
 function InfoUser() {
     return (
-        <div className="info-user">
+        <div className="info-user" id="whoAmI">
             <div className="info-user__title">
                 <p className="info-user__title">Who Am I?</p>
             </div>
-            <div className="info-user__photo">
+            {/* <div className="info-user__photo">
                 <img src={JhonPng} alt="Jhon" width="100%"/>
-            </div>
+            </div> */}
             <div className="info-user__description">
-                <p>A FullStack developer ..... a big text here</p>
+                <p>I am currently studying Computer Science and my focus is to learn and develop projects using the Java language on the back-end using Spring Framework and ReactJS on the front-end.</p>
+                <p><span>Skills: </span>Java, JavaScript, HTML, CSS, Bootstrap, ReactJS, React Native, Alteryx, SQL, MySql,
+SqlServer, GIT, GitFlow.</p>
+
             </div>
         </div>
     );
